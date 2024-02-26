a412eefe298c9081e96c538b341dbb30cde3d6e1
PaireCastor <jacques.pocino@yahoo.fr>
2024-02-26 19:48:39 +0100

    sbire2 le sbire mineur (de fond)

---
+++ Assets/Resources/Prefabs/Sbires/Sbire2.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using classes;
+using systems;
+
+namespace LesMeilleursScripts
+{
+    public class Sbire2 : Unit
+    {
+        //on experimente le minage
+        HealthSystem health;
+        public GameObject nexus, mine;
+        public RTSRessourceManager _rM;
+        public int loadCapacity = 10, load = 0;
+
+    public override void Awake()
+    {
+        health = GetComponent<HealthSystem>();
+        base.Awake();foreach(GameObject obj in FindObjectsOfType<GameObject>())
+        {
+            if (obj.name == "Nexus")
+            {
+                Debug.Log("miningSbire.cs : nexus found");
+                nexus = obj;
+            }
+            if (obj.name == "GoldMine")
+            {
+                Debug.Log("miningSbire.cs : GoldMine found");
+                mine = obj;
+            }
+            if (obj.name == "RTSManager")
+            {
+                Debug.Log("miningSbire.cs : RtsManager found");
+               _rM = obj.GetComponent<RTSRessourceManager>();
+            }
+        }
+        
+    } 
+
+    public override void AttackAction() 
+    {}
+
+    }
+}
