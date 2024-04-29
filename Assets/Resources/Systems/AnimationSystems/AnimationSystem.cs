6bfd849920e15d5258ac36e8c137c07c5f5d6fe7
Kwipse <nathan.polle@gmail.com>
2024-04-29 19:23:08 +0200

    Passement de jambe

---
+++ Assets/Resources/Systems/AnimationSystems/AnimationSystem.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace scriptablesobjects
+{
+    [CreateAssetMenu]
+    public class AnimationSystem : ScriptableObject
+    {
+        public Animator anim;
+    }
+}
